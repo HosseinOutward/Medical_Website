0a89c5ff998ec210ec54dc3888d4f54089a29379
Hossein <hossenmsignin@gmail.com>
2021-03-03 21:51:55 +0330

    ignore

--- Medical_Website/settings.py
+++ Medical_Website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "188.75.122.178"]
+ALLOWED_HOSTS = ["127.0.0.1", "188.75.122.178", "5.57.38.66"]
 
 
 # Application definition

62a8d43e38078c2398341bb4896d3c0a0ac43102
ali <aligholamzadeh42@gmail.com>
2021-04-30 01:11:19 +0430

    add label status

--- templates/static/js/H/listing_menu.js
+++ templates/static/js/H/listing_menu.js
@@ -22,7 +22,42 @@ for(i=0; i<image_list.length; i++) {
 
     html += '<td> <a href="/edit_image/'+image_list[i].pk+'/" type="submit" class="btn btn-info ">\n'
         +'<span >تغییر اطلاعات ورودی</span></a></td>';
+
+    html += '<td> <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#filterModal" onclick="getFilters('+ image_list[i].pk +')">' +
+            '<span>نمایش لیبل‌ها</span></button></td>';
     html += '</tr>';
 
     $('#Image_List').find('tbody').append(html);
+}
+
+function getFilters(indx) {
+    var apiURL='/_api/image/'+indx+'/'
+    var response = $.ajax({
+        url: apiURL,
+        dataType: 'json',
+        async: false,
+        type: 'GET',
+    });
+    const names = ["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10",
+                            "T11", "T12", "T13", "Max Aortic diameter dorsal" , "Max Aortic diameter ventral",
+                            "carina", "Apex", "Dorsal border of CVC", "ventral CVC", "Cranial", "Heart", "CVC", "Aorta", "region 1", "region 2", "region 3"]
+    var labels = JSON.parse(response.responseJSON.label_data_imag);
+    var body = '';
+    var data = {}
+    for (var name of names)
+        data[name] = [];
+    for (var label of labels) {
+        if (label.from_name) {
+            for (var l of label.value[label.type]) {
+                data[l].push(label.type);
+            }
+        }
+    }
+    for (const [name, type] of Object.entries(data)) {
+        body += '<tr>';
+        body += '<td>' + type + '</td>';
+        body += '<td>' + name + '</td>';
+        body += '</tr>';
+    }
+    $('#labelsList').find('tbody').html(body);
 }
